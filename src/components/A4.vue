fd95d6df5ab568ece7976613919f4922d1162f7c
chieh <email@zzj.cool>
2021-01-11 20:58:23 +0800

    '初始化提交'

---
+++ src/components/A4.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="a4">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'A4'
+}
+</script>
+
+<style lang="scss">
+
+.a4 {
+  max-width: 21cm;
+  min-height: 29.7cm;
+  padding: 2cm;
+  border-radius: 5px;
+  background: white;
+  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.4);
+  margin: 8mm auto;
+  @media (max-width: 810px) {
+    margin: 0;
+    border-radius: initial;
+    width: initial;
+    min-height: initial;
+    box-shadow: initial;
+  }
+}
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-page>
+    <a4>
+      <a href="#cd">产品介绍</a>
+      <router-link to="/#cd">
+        cd
+      </router-link>
+      <img
+        alt="Quasar logo"
+        src="~assets/quasar-logo-full.svg"
+      >
+    </a4>
+    <a4>
+      <div id="cd">cd</div>
+      <img
+        alt="Quasar logo"
+        src="~assets/quasar-logo-full.svg"
+      >
+    </a4>
+  </q-page>
+</template>
+
+<script>
+
+import A4 from 'components/A4'
+
+export default {
+  name: 'PageIndex',
+  components: { A4 }
+}
+</script>
