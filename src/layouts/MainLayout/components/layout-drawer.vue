ad4f24c4f853021b12913da46af18173bfff93ff
chieh <email@zzj.cool>
2021-01-13 14:31:47 +0800

    增加关于本页

--- src/layouts/MainLayout/components/layout-drawer.vue
+++ src/layouts/MainLayout/components/layout-drawer.vue
@@ -12,14 +12,40 @@
           为了保证打印效果，请使用Chromium内核浏览器(Chrome)
         </q-tooltip>
       </q-btn>
+      <q-separator spaced="12px" color="blue-grey-8"></q-separator>
+      <div class="q-gutter-y-md">
+        <div class="text-h5 text-weight-bold text-center">关于本页</div>
+        <div class="row items-center">
+          <div class="text-subtitle1 text-weight-bold">
+            技术栈:
+          </div>
+          <div>
+            <q-chip color="blue-grey-6" text-color="white">Vue</q-chip>
+            <q-chip color="blue-grey-6" text-color="white">Quasar</q-chip>
+          </div>
+        </div>
+        <div class="row items-center">
+          <div class="text-subtitle1 text-weight-bold">
+            项目地址:
+          </div>
+          <q-chip clickable color="blue-grey-9" text-color="white" icon="fab fa-github-alt"
+                  @click="openURL('https://github.com/CCChieh/cv-zzj')">
+            CCChieh/cv-zzj
+          </q-chip>
+        </div>
+        <div></div>
+      </div>
     </div>
   </q-drawer>
 </template>
 
 <script>
+import { openURL } from 'quasar'
+
 export default {
   name: 'LayoutDrawer',
   methods: {
+    openURL,
     print () {
       window.print()
     }
