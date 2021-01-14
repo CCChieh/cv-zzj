67e48207c83c4a82d985085949beb59ab09e5507
chieh <email@zzj.cool>
2021-01-14 20:19:43 +0800

    完善项目经历

--- src/infomation/project-list.js
+++ src/infomation/project-list.js
@@ -1,29 +1,31 @@
 // 相关网站的链接
 const projectList = [
   {
-    name: '华东师范大学水杉在线教育平',
+    name: '华东师范大学水杉在线教育平台',
     demo: 'https://www.shuishan.net.cn/',
     detail: '“水杉在线”是华东师范大学数据科学与工程学院推出的新一代数字化全链路在线学习平台，是一个面向学生“学”、“练”、“测”、“创”一体的综合性学习社区。',
     technology: [
-      'CI',
-      'Docker'
+      '前端：Vue+Quasar+Element UI；',
+      '后端：Spring Cloud+Shiro+Mybatis+Redis+MySql',
+      '采用微服务架构，完整的CI/CD支持，使用Docker进行打包部署'
     ],
     duty: [
-      '开发',
-      '运维'
+      '后端项目框架搭建',
+      '水杉在线前端开发',
+      'CI/CD实现，以及线上运维部署'
     ]
   },
   {
     name: '夏普解决方案网站',
     demo: 'http://sharp.next-lab.cn/',
-    detail: '在线教育平台',
+    // detail: '在线教育平台',
     technology: [
-      'CI',
-      'Docker'
+      '后端基于Spring Boot',
+      '前端采用Element UI，支持响应式布局'
     ],
     duty: [
-      '开发',
-      '运维'
+      '全栈开发',
+      '数据库设计'
     ]
   }
 ]
