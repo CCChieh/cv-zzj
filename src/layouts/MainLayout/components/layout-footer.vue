77d8b42e54ddb82ac69ffc8f9d201f83843d92e7
chieh <email@zzj.cool>
2021-01-14 20:33:43 +0800

    Footer

--- src/layouts/MainLayout/components/layout-footer.vue
+++ src/layouts/MainLayout/components/layout-footer.vue
@@ -1,12 +1,12 @@
 <template>
   <q-footer class="bg-blue-grey text-white print-hide">
-    <q-toolbar>
-      <q-toolbar-title>
-        <q-avatar>
-          <img src="~assets/head.png">
-        </q-avatar>
-        Title
-      </q-toolbar-title>
+    <q-toolbar class="column items-center justify-center">
+      <div>
+        本页面由<a href="https://github.com/CCChieh" target="_blank"><i>CCChieh</i></a>完成
+      </div>
+      <div>
+        项目地址<a href="https://github.com/CCChieh/cv-zzj" target="_blank"><i>https://github.com/CCChieh/cv-zzj</i></a>
+      </div>
     </q-toolbar>
   </q-footer>
 </template>
