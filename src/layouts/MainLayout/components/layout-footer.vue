67a40d285975a6062c7c5f651bffb2f417ee12e6
chieh <email@zzj.cool>
2021-01-14 20:44:10 +0800

    修改Footer

--- src/layouts/MainLayout/components/layout-footer.vue
+++ src/layouts/MainLayout/components/layout-footer.vue
@@ -1,8 +1,12 @@
 <template>
   <q-footer class="bg-blue-grey text-white print-hide">
-    <q-toolbar class="row justify-center">
-      <div>本页面由<a href="https://github.com/CCChieh" target="_blank"><i>CCChieh</i></a>完成，项目地址<a
-        href="https://github.com/CCChieh/cv-zzj" target="_blank"><i>https://github.com/CCChieh/cv-zzj</i></a></div>
+    <q-toolbar class="column items-center justify-center">
+      <div>
+        本页面由<a href="https://github.com/CCChieh" target="_blank"><i>CCChieh</i></a>完成
+      </div>
+      <div>
+        项目地址<a href="https://github.com/CCChieh/cv-zzj" target="_blank"><i>https://github.com/CCChieh/cv-zzj</i></a>
+      </div>
     </q-toolbar>
   </q-footer>
 </template>
